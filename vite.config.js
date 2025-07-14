27bcf857864272b11be01c0fb14411f49a7d8a16
Danishwara Pracheta <danishwara81@gmail.com>
2025-07-15 04:48:29 +0800

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         host: '0.0.0.0',
         strictPort: true,
         hmr: {
-            host: '192.168.1.8',
+            host: '192.168.1.20',
         },
     },
     plugins: [
